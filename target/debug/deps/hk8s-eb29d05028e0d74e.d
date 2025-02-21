a56742ffef929613d3460d51d4064e5d152d7950
AnthKta <anramosmd1@protonmail.com>
2025-02-20 22:43:42 -0500

    Reading a pod

---
+++ target/debug/deps/hk8s-eb29d05028e0d74e.d
@@ -0,0 +1,5 @@
+/home/antkta/Documents/hk8s/target/debug/deps/hk8s-eb29d05028e0d74e: src/main.rs
+
+/home/antkta/Documents/hk8s/target/debug/deps/hk8s-eb29d05028e0d74e.d: src/main.rs
+
+src/main.rs:
--- target/debug/hk8s.d
+++ target/debug/hk8s.d
@@ -1,1 +1,1 @@
-/home/antkta/hk8s/target/debug/hk8s: /home/antkta/hk8s/src/main.rs
+/home/antkta/Documents/hk8s/target/debug/hk8s: /home/antkta/Documents/hk8s/src/main.rs
